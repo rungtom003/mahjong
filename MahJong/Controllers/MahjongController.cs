ae1d502529509ba20d74195e9a76501d6de6a7ea
rungtom003 <rungchai990@gmail.com>
2020-11-09 08:56:28 +0700

    อัพเดทไฟล์ครั้งแรก

---
+++ MahJong/Controllers/MahjongController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MahJong.Controllers
+{
+    public class MahjongController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Storedetail()
+        {
+            return View();
+        }
+        public IActionResult Contact() {
+            return View();
+        }
+        public IActionResult Store()
+        {
+            return View();
+        }
+        public IActionResult Register()
+        {
+            return View();
+        }
+    }
+}
